95ed53a1592d950b113d185521ff24d11377a2fe
Daelox201 <kuimayor2@gmail.com>
2022-03-15 15:32:57 -0600

    Subo el main

---
+++ ProyectoCine/src/proyectocine/ProyectoCine.java
@@ -0,0 +1,17 @@
+
+package proyectocine;
+
+/**
+ *
+ * @author DAEL
+ */
+public class ProyectoCine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
